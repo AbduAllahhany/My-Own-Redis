ec6345c8eeaf98b82ed1d4e8a9964f258e779ef0
Abdallah Hany <abdallah.hany1974@gmail.com>
2025-07-31 17:12:09 +0300

    fix command propagation

--- app/server/server_command.go
+++ app/server/server_command.go
@@ -122,7 +122,7 @@ func Psync(request *Request) []byte {
 	out := "FULLRESYNC" + " " + request.Serv.Id + " " + strconv.Itoa(request.Serv.offset)
 	conn.Write(resp.SimpleStringDecoder(out))
 	// bgserverReplication(request)
-	res := resp.BulkStringDecoder("524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2")
+	res := resp.BulkStringDecoder("pk")
 	return res[:len(res)-2]
 }
 func bgserverReplication(request *Request) []byte {
